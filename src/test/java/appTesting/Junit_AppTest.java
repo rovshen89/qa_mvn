ccdc5f12dfc046e6aa23bb0fb803112dba8d03bd
rovshen89 <rovshen89@gmail.com>
2020-06-22 14:18:54 -0500

    Maven basic tests

--- src/test/java/appTesting/Junit_AppTest.java
+++ src/test/java/appTesting/Junit_AppTest.java
@@ -13,13 +13,14 @@ public class Junit_AppTest {
         int res;
         res = atest.sumNmbrs(1, 3);
         Assertions.assertEquals(5, res, "sum mismatch");
-        System.out.println("@testSum method");
+        System.out.println("@testSum method run");
     }
 
     @Test
     public void testString() {
         String result = atest.addString("Rov", "shen");
         Assertions.assertEquals(result, "Rov_shen", "String mismatch");
+        System.out.println("@testString method run");
     }
 
 

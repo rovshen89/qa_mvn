2b3771a722404f8527b5d78e9744b3b6fb427956
rovshen89 <rovshen89@gmail.com>
2020-06-22 13:49:44 -0500

    Maven basic tests

---
+++ src/main/java/appCode/AppforTest.java
@@ -0,0 +1,20 @@
+package appCode;
+
+public class AppforTest {
+
+    public int sumNmbrs(int a, int b) {
+        return a + b + 1;
+
+    }
+
+    public String addString(String a, String b) {
+        return a + "_" + b;
+    }
+
+    public int[] getArray() {
+        int[] arrEx = {1, 2, 3, 4};
+        return arrEx;
+    }
+
+
+}
---
+++ src/test/java/appTesting/Junit_AppTest.java
@@ -0,0 +1,26 @@
+package appTesting;
+
+import appCode.AppforTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Junit_AppTest {
+
+    AppforTest atest = new AppforTest();
+
+    @Test
+    public void testSum() {
+        int res;
+        res = atest.sumNmbrs(1, 3);
+        Assertions.assertEquals(5, res, "sum mismatch");
+        System.out.println("@testSum method");
+    }
+
+    @Test
+    public void testString() {
+        String result = atest.addString("Rov", "shen");
+        Assertions.assertEquals(result, "Rov_shen", "String mismatch");
+    }
+
+
+}
